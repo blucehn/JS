b67bf355f26edc3d75b3731c2fb8c346de38b8d4
lyking1990 <435521022@qq.com>
2016-08-26 18:26:12 +0800

    08-26

---
+++ Main/CreateList.js
@@ -0,0 +1,19 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+function CreateList(){
+    this.oWrap = document.createElement("div");
+}
+
+CreateList.prototype = {
+    initialize: function(aData){
+        
+        while(aData[0]) {
+            
+        }
+    }
+}
